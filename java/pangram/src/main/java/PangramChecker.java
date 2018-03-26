77cb4dcfda88c37659073f96392a22db46bc3585
Akshay Bijawe <akshaybijawe121@gmail.com>
2018-03-25 18:47:21 -0700

    Add the solution for pangram

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,21 @@
 public class PangramChecker {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        input = input.toLowerCase();
+
+        //a = 97 .... z = 122
+        boolean[] asciiValues = new boolean[123];
+
+        for(char c: input.toCharArray()){
+            asciiValues[(int)c] = true;
+        }
+
+        for(int i = 97; i < 123; i++){
+            if(asciiValues[i] == false){
+                return false;
+            }
+        }
+        return true;
     }
 
 }
