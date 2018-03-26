c1c23ff9132748f26efb339d88741ebbe2104ac6
Akshay Bijawe <akshaybijawe121@gmail.com>
2018-03-25 19:47:36 -0700

    Add the solution for hamming problem

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,11 +1,31 @@
+import java.lang.*;
+
 class Hamming {
 
-    Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	public String leftStrand;
+	public String rightStrand; 
+
+    Hamming(String leftStrand, String rightStrand) throws IllegalArgumentException {
+        if(leftStrand.length() != rightStrand.length()){
+        	throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }else{
+        	this.leftStrand = leftStrand;
+        	this.rightStrand = rightStrand;
+        	getHammingDistance();
+        }
     }
 
     int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    	int counter = 0;
+    	char[] leftStrandCharArray = leftStrand.toCharArray();
+    	char[] rightStrandCharArray = rightStrand.toCharArray();
+
+        for(int i = 0; i < leftStrand.length(); i++){
+        	if(leftStrandCharArray[i] != rightStrandCharArray[i]){
+        		counter++;
+        	}
+        }
+        return counter;
     }
 
 }
